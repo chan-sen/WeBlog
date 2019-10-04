343b6f57799841569540798abf169feb19e23b2b
Corey <corey@Coreys-MacBook.local>
2019-10-04 13:26:32 -0700

    Hello, world

--- src/app.py
+++ src/app.py
@@ -1,3 +1,14 @@
 __author__ = 'chansen'
 
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/') # www.mysite.com/api/
+def hello_method():
+    return "Hello, world!"
+
+
+if __name__ == '__main__':
+    app.run()
