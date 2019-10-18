b2cdad332a66dbfd6fac6ae23c461186160458a9
Corey <corey@Coreys-MacBook.local>
2019-10-18 02:03:47 -0700

    register page which adds to db

--- src/app.py
+++ src/app.py
@@ -9,9 +9,19 @@ app = Flask(__name__)
 app.secret_key = 'ggg'
 
 
-@app.route('/')                     # https://www.mysite.com/api/
-def hello_method():
+@app.route('/')
+def home_template():
+    return render_template('home.html')
+
+
+@app.route('/login')                     # https://www.mysite.com/api/
+def login_template():
     return render_template('login.html')
+
+
+@app.route('/register')                     # https://www.mysite.com/api/
+def register_template():
+    return render_template('register.html')
 
 
 @app.before_first_request
@@ -19,30 +29,44 @@ def initialize_database():
     Database.initialize()
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST'])
 def login_user():
+    # authorize login after HTML form submission
+    #   render user profile
     email = request.form['email']
     password = request.form['password']
 
     if User.login_valid(email, password):
         User.login(email)
         return render_template("profile.html", email=session['email'])
-    return hello_method()
+    else:
+        return register_template()
 
 
+@app.route('/auth/register', methods=['POST'])
+def register_new_user():
+    # authorize register after HTML form submission
+    #   render user profile
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
+    return render_template("profile.html", email=session['email'])
+
+
+"""
 @app.route('/profile', methods=['GET'])
 def user_profile():
+    # get and render user profile
     user = User.get_by_email(email=session['email'])
 
     blogs = user.get_blogs()
 
     return render_template("user_profile.html",
                            email=session['email'],
-                           blogs=blogs,
+                           blogs=blogs
                            )
-
-
-
+"""
 
 if __name__ == '__main__':
     app.run()
